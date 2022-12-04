8094705e4a188cfbe81ad0e31b7529386cd7b41a
Riyas Yacub <riyasyacub@Riyass-MacBook-Air.local>
2022-12-04 18:44:42 +0530

    issues resolved

--- app/javascript/controllers/staff_controller.js
+++ app/javascript/controllers/staff_controller.js
@@ -1,15 +1,27 @@
-import { Controller } from "@hotwired/stimulus"
+import {Controller} from "@hotwired/stimulus"
 
 // Connects to data-controller="staff"
 export default class extends Controller {
-  static targets = ['message', 'billing', 'iframe']
+    static targets = ['message', 'billing', 'iframe', 'details']
 
-  connect() {
-    console.log("connected")
-  }
+    connect() {
+        console.log("connected")
 
-  click(event) {
-    const url = event.target.dataset.url
-    this.iframeTarget.setAttribute("src", url)
-  }
+    }
+
+    click(event) {
+        const url = event.target.dataset.url
+        this.iframeTarget.setAttribute("src", url)
+    }
+
+    open(event) {
+        const id = event.target.dataset.blogId;
+        if (event.target.classList.contains('active')) {
+            event.target.classList.remove('active')
+            $("#"+id).slideToggle();
+        } else {
+            event.target.classList.add('active')
+            $("#"+id).slideToggle();
+        }
+    }
 }
